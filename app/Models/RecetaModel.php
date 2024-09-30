02d95748f520afe9b81719f243c6a77e9db40d1c
Gaby <gaby-saya@outlook.com>
2024-09-29 19:52:03 -0600

    Creacion de migraciones, modelo, controller

---
+++ app/Models/RecetaModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RecetaModel extends Model
+{
+    use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'intrucciones',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'id',
+        'edo',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'tipoAlimentoId' => 'int',
+            'edo' => 'active',
+        ];
+    }
+}
